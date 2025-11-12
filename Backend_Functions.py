5e5da70ddcbe0677796ddf4edfa663adc68f3278
Udaybansal10 <udaybansal1014@gmail.com>
2025-11-12 13:09:40 +0530

    Backend Utility Functions

---
+++ Backend_Functions.py
@@ -0,0 +1,10 @@
+from Backend import checkpointer
+
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
